06e0e58e8130dd76860d14a9038184f2c9de2a7b
Sunit43 <infobunny6@gmail.com>
2025-04-26 23:57:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+from rembg import remove
+import io
+
+app = FastAPI()
+
+@app.post("/remove-background/")
+async def remove_background(file: UploadFile = File(...)):
+    image_data = await file.read()
+    output_data = remove(image_data)
+    return StreamingResponse(io.BytesIO(output_data), media_type="image/png")
